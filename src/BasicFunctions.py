5664948d1040487d14bb97c0aafaaddbbcf387a3
pc <matyas.taubel@scioskola.cz>
2020-11-09 11:03:50 +0100

    subtract multiply and divide implemented

--- src/BasicFunctions.py
+++ src/BasicFunctions.py
@@ -1,25 +1,23 @@
 # Basic mathematical operations
 
 # Function to add two numbers
-def add(num1, num2) -> object:
+def add(num1, num2):
     return num1 + num2
 
 
 # Function to subtract two numbers
 def subtract(num1, num2):
-    return NotImplemented
+    return num1 - num2
 
 
 # Function to multiply two numbers
 def multiply(num1, num2):
-    return NotImplemented
+    return num1 * num2
 
 
 # Function to divide two numbers
 def divide(num1, num2):
-    return NotImplemented
+    return num1 / num2
 
 
-# Function to divide two numbers
-def divide(num1, num2):
-    return NotImplemented
+#zdravim
