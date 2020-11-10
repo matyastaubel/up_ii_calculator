69321ca67d9a4286a86b5d7ee68a4a56e976c32a
pc <matyas.taubel@scioskola.cz>
2020-11-10 14:31:16 +0100

    add 3 +  implented

--- tests/BasicFunctionsTest.py
+++ tests/BasicFunctionsTest.py
@@ -7,7 +7,7 @@ def add_test():
     unit_test(16, add, 10.4, 5.6)
     unit_test(-5, add, -10, 5)
     unit_test(5, add, 5, 0)
-
+    unit_test(10, add, 3, 7)
 
 # tests if subtract() works for zero, positive numbers and negative numbers
 def subtract_test():
