a8308fda95fbd81da17651f91fabde4b3352164c
pc <matyas.taubel@scioskola.cz>
2020-11-28 15:22:05 +0100

    def factorial(n): Implemented

--- src/AdvFunctions.py
+++ src/AdvFunctions.py
@@ -8,8 +8,13 @@ def int_division(num1, num2):
 
 
 # Returns factorial of given number
-def factorial(num):
-    return num * factorial(num - 1)
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+n=int(input("Input a number to complete the factorial : "))
+print(factorial(n))
 
 
 # Returns all solutions of given quadratic equation
